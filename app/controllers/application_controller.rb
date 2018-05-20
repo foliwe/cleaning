f9decb86a11390006dc37a1f77eca4d7ebd651d2
foliwe <foliwe@gmail.com>
2018-05-20 03:16:19 +0100

    styling1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_tenant!
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
      ##    milia defines a default max_tenants, invalid_tenant exception handling
      ##    but you can override these if you wish to handle directly
   rescue_from ::Milia::Control::MaxTenantExceeded, :with => :max_tenants
   rescue_from ::Milia::Control::InvalidTenantAccess, :with => :invalid_tenant
 
-  before_action :authenticate_tenant!
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:company_name])
+  end
 end

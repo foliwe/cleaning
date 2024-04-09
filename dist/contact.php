5bb447c8d7413b0609498da10cb424a1307c9392
foliwe <foliwe@gmail.com>
2024-04-09 12:49:28 +0100

    refactore

--- dist/contact.php
+++ dist/contact.php
@@ -117,7 +117,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
         <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
           <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-          <a class="text-teal-500 leading-relaxed">info@ryanhealthandcleaningltd.co.uk</a>
+          <a class="text-teal-500 leading-relaxed">info@ryanservicesltd.co.uk</a>
           <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
           <p class="leading-relaxed">+442088541211</p>
         </div>

b6a968638fd04042a544372d9d103abcfdc2d1e9
foliwe <foliwe@gmail.com>
2024-04-09 22:18:58 +0100

    refactore

--- dist/about.php
+++ dist/about.php
@@ -45,9 +45,16 @@
           <h1 class="text-4xl py-4 text-teal-500 font-bold">
             Why choose Us
           </h1>
-          <p class="pb-3">
-            We stand for excellence and commitment in an ever-changing world. Our unparalleled expertise, customer-centric approach, and relentless pursuit of perfection set us apart. We don't just offer services; we offer experiences tailored to your exact needs. Our team is not just skilled—they're passionate about providing solutions that make a tangible difference in your life. With state-of-the-art technology, innovative practices, and a track record of outstanding results, we go the extra mile to ensure your satisfaction. Our dedication to your success is unwavering, and our reputation as industry leaders is well-earned. Partner with us, and experience the highest standard of excellence.
-          </p>
+          
+          <p>At <strong>Ryan Services Limited</strong>, we're dedicated to more than just cleanliness; we're committed to your <span class="text-teal-500 font-bold">satisfaction.</span> Our team understands that a clean environment contributes to your overall happiness and well-being, which is why we go above and beyond to deliver exceptional service tailored to your exact needs.</p>
+
+          <p>With our <span class="text-teal-500 font-bold">Comprehensive Insurance</span> coverage, you can trust that your property is in safe hands. We take every precaution to ensure that our services are carried out with the utmost care and attention to detail, giving you peace of mind knowing that you're fully protected.</p>
+
+          <p>Furthermore, our <span class="text-teal-500 font-bold">Lowest Price Guarantee</span> ensures that you receive the best value for your money. We believe that quality cleaning shouldn't come at a premium, which is why we strive to offer competitive rates without compromising on the standard of service.</p>
+
+          <p>And of course, our <span class="text-teal-500 font-bold">Happiness Guarantee</span> is at the heart of everything we do. We're not satisfied until you're delighted with the results. Whether it's a one-time deep clean or regular maintenance, we're here to ensure that your experience with us leaves you with a smile on your face and a spotless space to enjoy.</p>
+
+          <p>Choose <strong>Ryan Services Limited</strong> for all your cleaning needs, and experience the difference that our dedication to excellence can make.</p>  
           <div class="inline-flex pb-3 space-x-2">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#10b981" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
@@ -86,24 +93,21 @@
 
       <div class="md:w-1/2 grid grid-cols-2 gap-4">
 
-        <div class="bg-teal-500 p-10 shadow-xl grid place-content-center text-center">
-          <h1 class="text-2xl text-white font-bold bg-teal-600 py-1 px-6 rounded-xl">100+</h1>
-            <p class="py-2 text-white">Services Done</p>
+        <div class="specs bg-teal-500 p-10 shadow-xl grid place-content-center text-center" data-delay="0s">
+          <h1 class="text-2xl text-white font-bold bg-teal-600 py-1 px-6 rounded-xl">100%</h1>
+            <p class="py-2 text-yellow-400 font-bold text-xl">Satisfaction</p>
         </div>
 
-        <div class="bg-teal-500 p-10 shadow-xl grid place-content-center text-center">
-          <h1 class="text-2xl text-white font-bold bg-teal-600 py-1 px-6 rounded-xl">200+</h1>
-            <p class="py-2 text-white">Total Clients</p>
+        <div class="specs bg-teal-500 p-10 shadow-xl grid place-content-center text-center" data-delay=".2s">
+            <p class="py-2 text-yellow-400 font-bold text-xl"> Comprehensive Insurance </p>
         </div>
 
-        <div class="bg-teal-500 p-10 shadow-xl grid place-content-center text-center">
-          <h1 class="text-2xl text-white font-bold bg-teal-600 py-1 px-6 rounded-xl">150+</h1>
-            <p class="py-2 text-white">Projects Done</p>
+        <div class="specs bg-teal-500 p-10 shadow-xl grid place-content-center text-center" data-delay=".4s">
+            <p class="py-2 text-yellow-400 font-bold text-xl">Happiness Guarantee </p>
         </div>
 
-        <div class="bg-teal-500 p-10 shadow-xl grid place-content-center text-center">
-          <h1 class="text-2xl text-white font-bold bg-teal-600 py-1 px-6 rounded-xl">10+</h1>
-            <p class="py-2 text-white">Years Of Experience</p>
+        <div class="specs bg-teal-500 p-10 shadow-xl grid place-content-center text-center" data-delay=".6s">
+            <p class="text-yellow-400 font-bold text-xl">Lowest Price Guarantee</p>
         </div>
         
 
@@ -112,5 +116,30 @@
   </div>
   </section>
   <?php include 'footer.php'; ?>
+
+  <script>
+
+      const boxes = document.querySelectorAll('.specs')
+      observer4 = new IntersectionObserver((entries) =>{
+
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0){
+        entry.target.style.animation = `boxanimation 1s ${entry.target.dataset.delay} forwards ease-out`;
+    }
+    else {
+        entry.target.style.animation = 'none'
+    }
+    }) 
+   
+    
+})
+
+
+boxes.forEach(box => {
+    observer4.observe(box)
+
+})
+
+</script>
 </body>
-</html>xl
+</html>
--- dist/services.php
+++ dist/services.php
@@ -39,7 +39,32 @@
         <div class="content container mx-auto flex flex-col justify-center md:flex-row p-5 md:p-20 gap-7 relative">
             
             <div class="bg-local p-5 bg-cover block md:h-auto h-60 w-full md:w-1/2 " style="background-image: url('images/clean.jpg')" >
-            <?php include 'includes/stars.php'; ?>
+            <div class="box flex flex-col bg-teal-500/90 text-white md:p-8 p-3 absolute top-5 left-10 md:left-40 shadow-2xl">
+    <p class="text-sm">Trusted Service</p>
+    <div class="flex">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="#facc15" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="#facc15" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="#facc15" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="#facc15" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="#facc15" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+        </svg>
+    </div>
+    <h1>100% Approved</h1>
+</div>
+
             </div>
             <div class=" flex flex-col mt-2 md:mt-0 gap-5">
                

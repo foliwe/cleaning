779ed26fa597c4ddc62b156b633f9c4e4fe51490
foliwe <foliwe@gmail.com>
2024-04-11 23:11:52 +0100

    refactor

--- dist/contact.php
+++ dist/contact.php
@@ -133,8 +133,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       </div>
     </div>
     <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-      <p class="leading-relaxed mb-5 text-gray-600">Contact us for inquiries and pricing </p>
+      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font"></h2>
+      <p class="leading-relaxed mb-5 text-gray-600">Fill out the form below for quotes or call us on <span class="font-bold text-xl text-teal-500"> +442088541211</span> </p>
        <?php if (!empty($errors)): ?>
             <div class="errors">
                 <?php foreach ($errors as $error): ?>
